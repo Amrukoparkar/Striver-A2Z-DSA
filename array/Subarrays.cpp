20242013dc5ac0f169eb452a4429176a86f18b15
Amruta koparkar <162043963+Amrukoparkar@users.noreply.github.com>
2026-04-02 21:14:56 +0530

    Create Subarrays.cpp

---
+++ array/Subarrays.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+int arithmeticSubarrays(vector<int> &a, int n) {
+    if (n < 3) return 0;
+
+    int ans = 0;
+    int curr = 0; // dp[i-1]
+
+    for (int i = 2; i < n; i++) {
+        if (a[i] - a[i-1] == a[i-1] - a[i-2]) {
+            curr = curr + 1;
+            ans += curr;
+        } else {
+            curr = 0;
+        }
+    }
+
+    return ans;
+}
