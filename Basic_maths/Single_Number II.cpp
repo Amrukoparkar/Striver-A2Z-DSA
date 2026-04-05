a3b66e5132b310001ea4b7cfb26dcb41ee252581
Amruta koparkar <162043963+Amrukoparkar@users.noreply.github.com>
2026-04-05 21:30:43 +0530

    Create Single_Number II.cpp

---
+++ Basic_maths/Single_Number II.cpp
@@ -0,0 +1,19 @@
+int elementThatAppearsOnce(vector<int> arr) {
+    int result = 0;
+
+    for (int i = 0; i < 32; i++) {
+        int bitCount = 0;
+
+        for (int num : arr) {
+            if (num & (1 << i)) {
+                bitCount++;
+            }
+        }
+
+        if (bitCount % 3 != 0) {
+            result |= (1 << i);
+        }
+    }
+
+    return result;
+}
