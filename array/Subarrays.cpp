26fad62583f84cfb3b48e397ed23a497131ded28
Amruta koparkar <162043963+Amrukoparkar@users.noreply.github.com>
2026-04-14 22:15:46 +0530

    Update Subarrays.cpp

--- array/Subarrays.cpp
+++ array/Subarrays.cpp
@@ -3,7 +3,7 @@ int arithmeticSubarrays(vector<int> &a, int n) {
     if (n < 3) return 0;
 
     int ans = 0;
-    int curr = 0; // dp[i-1]
+    int curr = 0; // dp[i- 1]
 
     for (int i = 2; i < n; i++) {
         if (a[i] - a[i-1] == a[i-1] - a[i-2]) {
